607027b3044887416357ff040dc8f0be1f7b503c
yooca <997195002@qq.com>
2021-04-09 14:53:50 +0800

    add file 3 trunk add

--- HiGit/src/main/java/hello.java
+++ HiGit/src/main/java/hello.java
@@ -3,5 +3,6 @@ public class hello {
         System.out.println("hellod");
         System.out.println("gengxing1");
         System.out.println("gengxing2-new branch");
+        System.out.println("gengxing3-trunk add");
     }
 }
