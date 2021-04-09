651d3575e113bec8987261aaae93a9f3eb492968
yooca <997195002@qq.com>
2021-04-09 14:43:25 +0800

    add file 2 new branch

--- HiGit/src/main/java/hello.java
+++ HiGit/src/main/java/hello.java
@@ -2,5 +2,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hellod");
         System.out.println("gengxing1");
+        System.out.println("gengxing2-new branch");
     }
 }
