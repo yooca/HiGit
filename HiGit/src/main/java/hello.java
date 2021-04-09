d7822faea010ac23ce72e066a6cd6aa2602101e2
yooca <997195002@qq.com>
2021-04-09 16:03:42 +0800

    Update hello.java

--- HiGit/src/main/java/hello.java
+++ HiGit/src/main/java/hello.java
@@ -4,5 +4,6 @@ public class hello {
         System.out.println("gengxing1");
         System.out.println("gengxing2-new branch");
         System.out.println("gengxing3-trunk add");
+        System.out.println("gengxing3-update online");
     }
 }
